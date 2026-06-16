4f774a2d7fbd06857eea39e9089503067fcac7aa
Renucopata <r.rengel99@gmail.com>
2026-06-16 17:32:08 -0400

    fix url

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://gym-mania-server.vercel.app/",
+  baseURL: "https://gym-mania-server.vercel.app/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
